17fdb31c7bfa8889afd3c0e363e39dd02158e915
lohnerj <lohnerj1@mymail.nku.edu>
2023-12-09 13:57:21 -0500

    Setting up structure

---
+++ main.py
@@ -0,0 +1,15 @@
+class MyProgram:
+    def __init__(self):
+        # Constructor, you can initialize attributes or perform setup here
+        print("Initializing MyProgram")
+
+    def run(self):
+        # Your program logic goes here
+        print("Running MyProgram")
+
+if __name__ == "__main__":
+    # Create an object of MyProgram
+    my_program_instance = MyProgram()
+
+    # Call the run method to execute the program logic
+    my_program_instance.run()
